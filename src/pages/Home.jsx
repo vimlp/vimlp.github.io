7340674e63729c1bb69a4d948b2925c5696d1bde
Phuong Thao <ngphthao031028@gmail.com>
2025-02-18 10:50:30 +0700

    quai

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
                 <img className='w-[500px] md:max-w-[480px]' src={assets.homepage_img} alt="" />
                 <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
                     <p>ViMLP (Vietnamese Minority Languages Preservation) is a pioneering initiative aimed at preserving and revitalizing minority languages in Vietnam through state-of-the-art speech-to-text translation works. This project introduces a comprehensive dataset and methodology for direct speech-to-text translation. Initially, we work on the Muong language - one of Vietnam's significant minority languages without a standardized writing system.</p>
-                    {/* <b className='text-gray-800'>Our Mission</b>
-                    <p>Our mission at Forever is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.</p> */}
+
+                    {/* <p>Our mission at Forever is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.</p>  */}
                 </div>
             </div>
             {/* <div className='text-4xl py-4'>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/VIMLP.GITHUB.IO',
+  base: '/vimlp.github.io/',
   plugins: [react()],
 })

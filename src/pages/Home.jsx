1c0754e688728cf743db2dd1e03b34d220ad4078
Phuong Thao <ngphthao031028@gmail.com>
2025-02-18 10:15:44 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,21 +6,17 @@ const Home = () => {
     return (
         <div>
             <div className='text-2xl text-center pt-8 border-t'>
-                <Title text1={'ABOUT'} text2={'US'} />
+                <Title text1={'ViMLP - '} text2={'Vietnamese Minority Languages Preservation'} />
             </div>
             <div className='my-1 flex flex-col md:flex-row gap-16'>
-                <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+                <img className='w-[500px] md:max-w-[480px]' src={assets.homepage_img} alt="" />
                 <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
-                    <p>Forever was born out of a passion for innovation and a desire to revolutionize the way
-                        people shop online. Our journey began with a simple idea: to provide a platform where
-                        customers can easily discover, explore, and purchase a wide range of products from the
-                        comfort of their homes.</p>
-                    <p>Since our inception, we've worked tirelessly to curate a diverse selection of high-quality products that cater to every taste and preference. From fashion and beauty to electronics and home essentials, we offer an extensive collection sourced from trusted brands and suppliers.</p>
-                    <b className='text-gray-800'>Our Mission</b>
-                    <p>Our mission at Forever is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.</p>
+                    <p>ViMLP (Vietnamese Minority Languages Preservation) is a pioneering initiative aimed at preserving and revitalizing minority languages in Vietnam through state-of-the-art speech-to-text translation works. This project introduces a comprehensive dataset and methodology for direct speech-to-text translation. Initially, we work on the Muong language - one of Vietnam's significant minority languages without a standardized writing system.</p>
+                    {/* <b className='text-gray-800'>Our Mission</b>
+                    <p>Our mission at Forever is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.</p> */}
                 </div>
             </div>
-            <div className='text-4xl py-4'>
+            {/* <div className='text-4xl py-4'>
                 <Title text1={'WHY'} text2={'CHOOSE US'} />
             </div>
             <div className='flex flex-col md:flex-row test-sm mb-20'>
@@ -36,7 +32,7 @@ const Home = () => {
                     <b>Exceptional Customer Service:</b>
                     <p className='text-gray-600'>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.</p>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
